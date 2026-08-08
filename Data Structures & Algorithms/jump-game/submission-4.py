7ffddcd4da050e167b9ed0289109885540754231
dpschibul <pschibul.dennis@gmail.com>
2026-08-08 21:59:04 +0300

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        memo = {}
+
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i >= len(nums)-1:
+                memo[i] = True
+                return True
+            
+            max_jump = nums[i]
+
+            for jump in range(1, max_jump + 1):
+                if dfs(jump + i):
+                    memo[i] = True
+                    return True
+            memo[i] = False
+            return False
+        
+        return dfs(0)
+
+        
