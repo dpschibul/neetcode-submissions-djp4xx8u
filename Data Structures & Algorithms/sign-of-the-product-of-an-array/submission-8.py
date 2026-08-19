995c4c8d8087e8d239eb3ec2ee81be1356a298b5
dpschibul <pschibul.dennis@gmail.com>
2026-08-19 13:49:56 +0200

    Add: sign-of-the-product-of-an-array - submission-8

---
+++ Data Structures & Algorithms/sign-of-the-product-of-an-array/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        count_neg = 0
+        product = 1
+        for n in nums:
+            product *= n
+            if n < 0:
+                count_neg +=1
+            if n == 0:
+                return 0 
+
+            
+        
+
+        return -1 if count_neg % 2 == 1 else 1
+        
