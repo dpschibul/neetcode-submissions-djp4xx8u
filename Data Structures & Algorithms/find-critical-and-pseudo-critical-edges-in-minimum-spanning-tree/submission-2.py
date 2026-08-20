160a313bd9c4332ddd370fd32dc56aa3915800dd
dpschibul <pschibul.dennis@gmail.com>
2026-08-20 15:22:45 +0200

    Add: find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree - submission-2

---
+++ Data Structures & Algorithms/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/submission-2.py
@@ -0,0 +1,68 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+    
+    def find(self, v1):
+        while v1 != self.parent[v1]:
+            self.parent[v1] = self.parent[self.parent[v1]]
+            v1 = self.parent[v1]
+        return v1
+
+    def union(self, u, v):
+        p1, p2 = self.find(u), self.find(v)
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
+
+
+class Solution:
+    def findCriticalAndPseudoCriticalEdges(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        for i, e in enumerate(edges):
+            e.append(i)
+        
+        edges.sort(key=lambda e: e[2])
+
+        mst_weight = 0
+
+        uf = UnionFind(n)
+
+        for v1, v2, w, i in edges:
+            if uf.union(v1, v2):
+                mst_weight += w
+        
+        critical, pseudo = [], []
+
+        for n1, n2, e_weight, i in edges:
+            weight = 0
+            uf = UnionFind(n)
+            for v1, v2, w, j in edges:
+                if i != j and uf.union(v1, v2):
+                    weight += w
+            if max(uf.rank) != n or weight > mst_weight:
+                critical.append(i)
+                continue
+            
+            uf = UnionFind(n)
+            weight = e_weight
+            uf.union(n1, n2)
+
+            for v1, v2, w, j in edges:
+                if uf.union(v1, v2):
+                    weight += w
+            if weight == mst_weight:
+                pseudo.append(i)
+            
+        
+        return [critical, pseudo]
+            
+
+
+
